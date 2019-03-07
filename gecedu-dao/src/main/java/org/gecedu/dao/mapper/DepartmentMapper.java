6b3ac2253780f4c4b11fa431f513fcb104a04dff
zhiming <123@java.com>
2019-03-07 15:07:20 +0800

    提交了dao代码

---
+++ gecedu-dao/src/main/java/org/gecedu/dao/mapper/DepartmentMapper.java
@@ -0,0 +1,9 @@
+package org.gecedu.dao.mapper;
+
+import org.gecedu.dao.model.Department;
+import com.github.abel533.mapper.Mapper;
+
+public interface DepartmentMapper extends Mapper<Department> {
+
+
+}
---
+++ gecedu-dao/src/main/java/org/gecedu/dao/model/Department.java
@@ -0,0 +1,43 @@
+package org.gecedu.dao.model;
+
+public class Department {
+    private Integer id;
+
+    private Integer parentId;
+
+    private String name;
+
+    private Integer orderNum;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getOrderNum() {
+        return orderNum;
+    }
+
+    public void setOrderNum(Integer orderNum) {
+        this.orderNum = orderNum;
+    }
+}
